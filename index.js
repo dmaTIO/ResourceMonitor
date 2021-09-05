07e987cbb30d2cbcb9f6665d7819644f7adefaa1
Dims <dmarkitanis@gmail.com>
2021-09-05 23:14:04 +0300

    v2

--- index.js
+++ index.js
@@ -1,196 +1,233 @@
 const ipc = require("electron").ipcRenderer;
 
 const formatTime = (s) => {
-	var date = new Date(s);
-	// Hours part from the timestamp
-	var hours = date.getHours();
-	// Minutes part from the timestamp
-	var minutes = "0" + date.getMinutes();
-	// Seconds part from the timestamp
-	var seconds = "0" + date.getSeconds();
+  var date = new Date(s);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
 
-	// Will display time in 10:30:23 format
-	return hours + ":" + minutes.substr(-2);
+  // Will display time in 10:30:23 format
+  return hours + ":" + minutes.substr(-2);
 };
 
 ipc.on("systemInfoOS", (event, data) => {
-	window.localStorage.setItem("systemInfoOS", data);
-	const osInfo = JSON.parse(data);
-	document.getElementById("pcName").innerHTML = osInfo.hostname + " - " + osInfo.distro;
+  window.localStorage.setItem("systemInfoOS", data);
+  const osInfo = JSON.parse(data);
+  document.getElementById("pcName").innerHTML =
+    osInfo.hostname + " - " + osInfo.distro;
 });
 
 ipc.on("mem", (event, data) => {
-	window.localStorage.setItem("mem", data);
-	const memInfo = JSON.parse(data);
-	document.getElementById("ramTotal").innerHTML = (parseFloat(memInfo.total) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
+  window.localStorage.setItem("mem", data);
+  const memInfo = JSON.parse(data);
+  document.getElementById("ramTotal").innerHTML =
+    (parseFloat(memInfo.total) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
 });
 
 ipc.on("systemTime", (event, data) => {
-	const currentDateTime = data.current;
-	document.getElementById("time").innerHTML = formatTime(currentDateTime);
+  const currentDateTime = data.current;
+  document.getElementById("time").innerHTML = formatTime(currentDateTime);
 });
 
 ipc.on("displays", (event, data) => {
-	const displaysArray = data;
-	window.localStorage.setItem("displaysArray", JSON.stringify(displaysArray));
-	document.getElementById("displaysCount").innerHTML = data.length;
+  const displaysArray = data;
+  window.localStorage.setItem("displaysArray", JSON.stringify(displaysArray));
+  document.getElementById("displaysCount").innerHTML = data.length;
 });
 
 ipc.on("GPUmemoryTotal", (event, data) => {
-	const GPUmemoryTotal = data;
-	window.localStorage.setItem("GPUmemoryTotal", JSON.stringify(GPUmemoryTotal));
+  const GPUmemoryTotal = data;
+  window.localStorage.setItem("GPUmemoryTotal", JSON.stringify(GPUmemoryTotal));
 });
 
 ipc.on("gpuName", (event, data) => {
-	const gpuName = data;
-	window.localStorage.setItem("gpuName", JSON.stringify(gpuName));
-	document.getElementById("gpuName").innerHTML = gpuName;
+  const gpuName = data;
+  window.localStorage.setItem("gpuName", JSON.stringify(gpuName));
+  document.getElementById("gpuName").innerHTML = gpuName;
 });
 
 ipc.on("bus", (event, data) => {
-	const bus = data;
-	window.localStorage.setItem("bus", JSON.stringify(bus));
+  const bus = data;
+  window.localStorage.setItem("bus", JSON.stringify(bus));
 });
 
 ipc.on("driverVersion", (event, data) => {
-	const driverVersion = data;
-	window.localStorage.setItem("driverVersion", JSON.stringify(driverVersion));
+  const driverVersion = data;
+  window.localStorage.setItem("driverVersion", JSON.stringify(driverVersion));
 });
 
 ipc.on("gpuCount", (event, data) => {
-	const gpuCount = data;
-	window.localStorage.setItem("gpuCount", gpuCount);
-	document.getElementById("gpuCount").innerHTML = gpuCount;
+  const gpuCount = data;
+  window.localStorage.setItem("gpuCount", gpuCount);
+  document.getElementById("gpuCount").innerHTML = gpuCount;
 });
 
 ipc.on("cpuInfo", (event, data) => {
-	const cpuInfo = data;
-	window.localStorage.setItem("cpuInfo", JSON.stringify(cpuInfo));
-	document.getElementById("cpuCount").innerHTML = data.cores;
-	document.getElementById("cpuName").innerHTML = data?.brand;
-	document.getElementById("socket").innerHTML = "Socket " + data?.socket;
+  const cpuInfo = data;
+  window.localStorage.setItem("cpuInfo", JSON.stringify(cpuInfo));
+  document.getElementById("cpuCount").innerHTML = data.cores;
+  document.getElementById("cpuName").innerHTML = data?.brand;
+  document.getElementById("socket").innerHTML = "Socket " + data?.socket;
 });
 
 ipc.on("currentLoad", (event, data) => {
-	const currentLoad = data;
-	// console.log(currentLoad);
-	document.getElementById("cpuPercentageValue").innerHTML = currentLoad.currentLoad.toFixed(2) + "%";
-	document.getElementById("cpuPercentageBar").style.width = currentLoad.currentLoad.toFixed(2) + "%";
+  const currentLoad = data;
+  // console.log(currentLoad);
+  document.getElementById("cpuPercentageValue").innerHTML =
+    currentLoad.currentLoad.toFixed(2) + "%";
+  document.getElementById("cpuPercentageBar").style.width =
+    currentLoad.currentLoad.toFixed(2) + "%";
 });
 
 ipc.on("gpuLoad", (event, data) => {
-	const gpuLoad = data;
-	document.getElementById("gpuPercentageValue").innerHTML = gpuLoad.toFixed(2) + "%";
-	document.getElementById("gpuPercentageBar").style.width = gpuLoad.toFixed(2) + "%";
+  const gpuLoad = data;
+  document.getElementById("gpuPercentageValue").innerHTML =
+    gpuLoad.toFixed(2) + "%";
+  document.getElementById("gpuPercentageBar").style.width =
+    gpuLoad.toFixed(2) + "%";
 });
 
 ipc.on("disks", (event, data) => {
-	const disksInfo = data;
-	window.localStorage.setItem("disks", JSON.stringify(disksInfo));
-	const diskBoxArray = [].slice.call(document.getElementById("drivesBox").children);
-	diskBoxArray.forEach((el, idx) => {
-		if (data[idx]) {
-			el.getElementsByClassName("name")[0].innerHTML = data[idx]["fs"] + "/";
-			el.getElementsByClassName("message-line")[0].innerHTML = (parseFloat(data[idx]["size"]) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
-			el.getElementsByClassName("message-line")[1].innerHTML = (parseFloat(data[idx]["available"]) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
-		} else {
-			el.remove();
-		}
-	});
+  const disksInfo = data;
+  console.log(data);
+  window.localStorage.setItem("disks", JSON.stringify(disksInfo));
+  const diskBoxArray = [].slice.call(
+    document.getElementById("drivesBox").children
+  );
+  diskBoxArray.forEach((el, idx) => {
+    if (data[idx]) {
+      el.getElementsByClassName("name")[0].innerHTML = data[idx]["fs"] + "/";
+      el.getElementsByClassName("message-line")[0].innerHTML =
+        (parseFloat(data[idx]["size"]) * (9.31 * Math.pow(10, -10))).toFixed(
+          2
+        ) + " GB";
+      el.getElementsByClassName("message-line")[1].innerHTML =
+        (
+          parseFloat(data[idx]["available"]) *
+          (9.31 * Math.pow(10, -10))
+        ).toFixed(2) + " GB";
+    } else {
+      el.remove();
+    }
+  });
 });
 
 //Dashboard UI functions
 
 document.addEventListener("DOMContentLoaded", function () {
-	//Get Name
-	const osInfoStored = JSON.parse(window.localStorage.getItem("systemInfoOS"));
-	document.getElementById("pcName").innerHTML = osInfoStored.hostname + " - " + osInfoStored.distro;
+  //Get Name
+  const osInfoStored = JSON.parse(window.localStorage.getItem("systemInfoOS"));
+  document.getElementById("pcName").innerHTML =
+    osInfoStored?.hostname + " - " + osInfoStored?.distro;
 
-	//Get Ram Total
-	const memInfo = JSON.parse(window.localStorage.getItem("mem"));
-	document.getElementById("ramTotal").innerHTML = (parseFloat(memInfo.total) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
+  //Get Ram Total
+  const memInfo = JSON.parse(window.localStorage.getItem("mem"));
+  document.getElementById("ramTotal").innerHTML =
+    (parseFloat(memInfo?.total) * (9.31 * Math.pow(10, -10))).toFixed(2) +
+    " GB";
 
-	//Get Displays
-	const displaysInfo = JSON.parse(window.localStorage.getItem("displaysArray"));
-	document.getElementById("displaysCount").innerHTML = displaysInfo.length;
+  //Get Displays
+  const displaysInfo = JSON.parse(window.localStorage.getItem("displaysArray"));
+  document.getElementById("displaysCount").innerHTML = displaysInfo?.length;
 
-	//Get GpuCount
-	const gpuCount = JSON.parse(window.localStorage.getItem("gpuCount"));
-	document.getElementById("gpuCount").innerHTML = gpuCount;
+  //Get GpuCount
+  const gpuCount = JSON.parse(window.localStorage.getItem("gpuCount"));
+  document.getElementById("gpuCount").innerHTML = gpuCount;
 
-	//Get GpuName
-	const gpuName = JSON.parse(window.localStorage.getItem("gpuName"));
-	document.getElementById("gpuName").innerHTML = gpuName;
+  //Get GpuName
+  const gpuName = JSON.parse(window.localStorage.getItem("gpuName"));
+  document.getElementById("gpuName").innerHTML = gpuName;
 
-	//Get GpuTotalMem
-	const gpuTotalMemory = JSON.parse(window.localStorage.getItem("GPUmemoryTotal"));
-	document.getElementById("gpuTotalMemory").innerHTML = "VRAM " + parseFloat(gpuTotalMemory) / 1000 + " GB";
+  //Get GpuTotalMem
+  const gpuTotalMemory = JSON.parse(
+    window.localStorage.getItem("GPUmemoryTotal")
+  );
+  document.getElementById("gpuTotalMemory").innerHTML =
+    "VRAM " + parseFloat(gpuTotalMemory) / 1000 + " GB";
 
-	//Get CPU Cores Count
-	const cpuInfo = JSON.parse(window.localStorage.getItem("cpuInfo"));
-	document.getElementById("cpuCount").innerHTML = cpuInfo?.cores;
-	document.getElementById("cpuName").innerHTML = cpuInfo?.brand;
-	document.getElementById("socket").innerHTML = "Socket " + cpuInfo?.socket;
+  //Get CPU Cores Count
+  const cpuInfo = JSON.parse(window.localStorage.getItem("cpuInfo"));
+  document.getElementById("cpuCount").innerHTML = cpuInfo?.cores;
+  document.getElementById("cpuName").innerHTML = cpuInfo?.brand;
+  document.getElementById("socket").innerHTML = "Socket " + cpuInfo?.socket;
 
-	//Get File System
-	const fileSystem = JSON.parse(window.localStorage.getItem("disks"));
-	const diskBoxArray = [].slice.call(document.getElementById("drivesBox").children);
-	diskBoxArray.forEach((el, idx) => {
-		if (fileSystem[idx]) {
-			el.getElementsByClassName("name")[0].innerHTML = fileSystem[idx]["fs"] + "/";
-			el.getElementsByClassName("message-line")[0].innerHTML = (parseFloat(fileSystem[idx]["size"]) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
-			el.getElementsByClassName("message-line")[1].innerHTML = (parseFloat(fileSystem[idx]["available"]) * (9.31 * Math.pow(10, -10))).toFixed(2) + " GB";
-		} else {
-			el.remove();
-		}
-	});
+  //Get File System
+  const fileSystem = JSON.parse(window.localStorage.getItem("disks"));
+  const diskBoxArray = [].slice.call(
+    document.getElementById("drivesBox").children
+  );
+  diskBoxArray.forEach((el, idx) => {
+    if (fileSystem[idx]) {
+      el.getElementsByClassName("name")[0].innerHTML =
+        fileSystem?.[idx]["fs"] + "/";
+      el.getElementsByClassName("message-line")[0].innerHTML =
+        (
+          parseFloat(fileSystem[idx]["size"]) *
+          (9.31 * Math.pow(10, -10))
+        ).toFixed(2) + " GB";
+      el.getElementsByClassName("message-line")[1].innerHTML =
+        (
+          parseFloat(fileSystem[idx]["available"]) *
+          (9.31 * Math.pow(10, -10))
+        ).toFixed(2) + " GB";
+    } else {
+      el.remove();
+    }
+  });
 
-	//Get Dark Mode
-	let isDarkMode = window.localStorage.getItem("darkMode") === "true";
-	var modeSwitch = document.querySelector(".mode-switch");
+  //Get Dark Mode
+  let isDarkMode = window.localStorage.getItem("darkMode") === "true";
+  var modeSwitch = document.querySelector(".mode-switch");
 
-	if (isDarkMode) {
-		document.documentElement.classList.add("dark");
-		modeSwitch.classList.add("active");
-	} else {
-		document.documentElement.classList.remove("dark");
-		modeSwitch.classList.remove("active");
-	}
+  if (isDarkMode) {
+    document.documentElement.classList.add("dark");
+    modeSwitch.classList.add("active");
+  } else {
+    document.documentElement.classList.remove("dark");
+    modeSwitch.classList.remove("active");
+  }
 
-	modeSwitch.addEventListener("click", function () {
-		document.documentElement.classList.toggle("dark");
-		modeSwitch.classList.toggle("active");
-		console.log();
-		if (modeSwitch.classList[1]) {
-			window.localStorage.setItem("darkMode", "true");
-		} else {
-			window.localStorage.setItem("darkMode", "false");
-		}
-	});
+  modeSwitch.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+    modeSwitch.classList.toggle("active");
+    console.log();
+    if (modeSwitch.classList[1]) {
+      window.localStorage.setItem("darkMode", "true");
+    } else {
+      window.localStorage.setItem("darkMode", "false");
+    }
+  });
 
-	var listView = document.querySelector(".list-view");
-	var gridView = document.querySelector(".grid-view");
-	var projectsList = document.querySelector(".project-boxes");
+  var listView = document.querySelector(".list-view");
+  var gridView = document.querySelector(".grid-view");
+  var projectsList = document.querySelector(".project-boxes");
 
-	listView.addEventListener("click", function () {
-		gridView.classList.remove("active");
-		listView.classList.add("active");
-		projectsList.classList.remove("jsGridView");
-		projectsList.classList.add("jsListView");
-	});
+  listView.addEventListener("click", function () {
+    gridView.classList.remove("active");
+    listView.classList.add("active");
+    projectsList.classList.remove("jsGridView");
+    projectsList.classList.add("jsListView");
+  });
 
-	gridView.addEventListener("click", function () {
-		gridView.classList.add("active");
-		listView.classList.remove("active");
-		projectsList.classList.remove("jsListView");
-		projectsList.classList.add("jsGridView");
-	});
+  gridView.addEventListener("click", function () {
+    gridView.classList.add("active");
+    listView.classList.remove("active");
+    projectsList.classList.remove("jsListView");
+    projectsList.classList.add("jsGridView");
+  });
 
-	document.querySelector(".messages-btn").addEventListener("click", function () {
-		document.querySelector(".messages-section").classList.add("show");
-	});
+  document
+    .querySelector(".messages-btn")
+    .addEventListener("click", function () {
+      document.querySelector(".messages-section").classList.add("show");
+    });
 
-	document.querySelector(".messages-close").addEventListener("click", function () {
-		document.querySelector(".messages-section").classList.remove("show");
-	});
+  document
+    .querySelector(".messages-close")
+    .addEventListener("click", function () {
+      document.querySelector(".messages-section").classList.remove("show");
+    });
 });
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, screen } = require("electron");
+const { app, BrowserWindow, screen, Menu } = require("electron");
 app.disableHardwareAcceleration();
 app.disableDomainBlockingFor3DAPIs();
 const path = require("path");
@@ -11,165 +11,187 @@ const si = require("systeminformation");
 
 // let resourceInterval;
 let popWindow = true;
+let menuTemplate = [
+  //   {
+  //     label: "Window Manager",
+  //     submenu: [],
+  //   },
+  //   {
+  //     label: "View",
+  //     submenu: [{ role: "reload" }, { label: "custom reload" }],
+  //   },
+];
 
 const createWindow = (screenX, screenY) => {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 900,
-		// resizable: false,
-		// alwaysOnTop: true,
-		// frame: false,
-		// x: screenX,
-		// y: screenY,
-		icon: "assets/icons/MBAM.ico",
-		minWidth: 900,
-		webPreferences: {
-			nodeIntegration: true,
-			nativeWindowOpen: true,
-			contextIsolation: false,
-			preload: path.join(__dirname, "preload.js"),
-		},
-	});
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
+    // resizable: false,
+    // alwaysOnTop: true,
+    // frame: false,
+    // x: screenX,
+    // y: screenY,
+    icon: "assets/icons/MBAM.ico",
+    minWidth: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      nativeWindowOpen: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-	const resourceInterval = setInterval(() => {
-		//Send System Time
-		mainWindow.webContents.send("systemTime", si.time());
+  const resourceInterval = setInterval(() => {
+    //Send System Time
+    mainWindow.webContents.send("systemTime", si.time());
 
-		//CPU Info
-		si.currentLoad()
-			.then((currentLoad) => {
-				mainWindow.webContents.send("currentLoad", currentLoad);
-			})
-			.catch((err) => {
-				console.log("currentLoad info error", err);
-			});
+    //CPU Info
+    si.currentLoad()
+      .then((currentLoad) => {
+        mainWindow.webContents.send("currentLoad", currentLoad);
+      })
+      .catch((err) => {
+        console.log("currentLoad info error", err);
+      });
 
-		//GPU Load Info
-		si.graphics()
-			.then((graphics) => {
-				// console.log(graphics);
-				const { controllers } = graphics;
-				const gpuLoad = controllers[0].utilizationGpu ? controllers[0].utilizationGpu : 0;
-				mainWindow.webContents.send("gpuLoad", gpuLoad);
-			})
-			.catch((err) => {
-				console.log("graphics info error", err);
-			});
-	}, 1000);
+    //GPU Load Info
+    si.graphics()
+      .then((graphics) => {
+        // console.log(graphics);
+        const { controllers } = graphics;
+        const gpuLoad = controllers[0].utilizationGpu
+          ? controllers[0].utilizationGpu
+          : 0;
+        mainWindow.webContents.send("gpuLoad", gpuLoad);
+      })
+      .catch((err) => {
+        console.log("graphics info error", err);
+      });
+  }, 1000);
 
-	// and load the index.html of the app.
-	mainWindow.loadFile("index.html");
+  // and load the index.html of the app.
+  mainWindow.loadFile("index.html");
+  //   let menu = Menu.buildFromTemplate(menuTemplate);
+  //   Menu.setApplicationMenu(menu);
 
-	// mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
-	mainWindow.on("closed", () => {
-		popWindow = null;
-		// clearInterval(resourceInterval);
-	});
+  mainWindow.on("closed", () => {
+    popWindow = null;
+    // clearInterval(resourceInterval);
+  });
 
-	mainWindow.on("close", () => {
-		popWindow = null;
-		// clearInterval(resourceInterval);
-	});
+  mainWindow.on("close", () => {
+    popWindow = null;
+    // clearInterval(resourceInterval);
+  });
+  mainWindow.on("ready-to-show", () => {
+    // System Info
+    si.osInfo()
+      .then((os) => {
+        // console.log(os);
+        const { distro, hostname, fqdn, platform } = os;
 
-	// System Info
-	si.osInfo()
-		.then((os) => {
-			// console.log(os);
-			const { distro, hostname, fqdn, platform } = os;
+        mainWindow.webContents.send("systemInfoOS", JSON.stringify(os));
+      })
+      .catch((err) => {
+        console.log("systemInfoOS info error", err);
+      });
 
-			mainWindow.webContents.send("systemInfoOS", JSON.stringify(os));
-		})
-		.catch((err) => {
-			console.log("systemInfoOS info error", err);
-		});
+    //Ram Info
+    si.mem()
+      .then((mem) => {
+        mainWindow.webContents.send("mem", JSON.stringify(mem));
+      })
+      .catch((err) => {
+        console.log("mem info error", err);
+      });
 
-	//Ram Info
-	si.mem()
-		.then((mem) => {
-			mainWindow.webContents.send("mem", JSON.stringify(mem));
-		})
-		.catch((err) => {
-			console.log("mem info error", err);
-		});
+    //Graphics Info
+    si.graphics()
+      .then((graphics) => {
+        // console.log(graphics);
+        const { displays } = graphics;
+        mainWindow.webContents.send("displays", displays);
+        //   console.log(displays);
+        mainWindow.webContents.send("gpuName", graphics.controllers[0].model);
+        mainWindow.webContents.send(
+          "GPUmemoryTotal",
+          graphics.controllers[0].memoryTotal ?? "0"
+        );
+        mainWindow.webContents.send("bus", graphics.controllers[0].bus);
+        mainWindow.webContents.send(
+          "driverVersion",
+          graphics.controllers[0].driverVersion
+        );
+        mainWindow.webContents.send("gpuCount", graphics.controllers.length);
+      })
+      .catch((err) => {
+        console.log("graphics info error", err);
+      });
 
-	//Graphics Info
-	si.graphics()
-		.then((graphics) => {
-			// console.log(graphics);
-			const { displays } = graphics;
-			mainWindow.webContents.send("displays", displays);
-			mainWindow.webContents.send("gpuName", graphics.controllers[0].model);
-			mainWindow.webContents.send("GPUmemoryTotal", graphics.controllers[0].memoryTotal);
-			mainWindow.webContents.send("bus", graphics.controllers[0].bus);
-			mainWindow.webContents.send("driverVersion", graphics.controllers[0].driverVersion);
-			mainWindow.webContents.send("gpuCount", graphics.controllers.length);
-		})
-		.catch((err) => {
-			console.log("graphics info error", err);
-		});
+    //CPU Info
+    si.cpu()
+      .then((cpu) => {
+        mainWindow.webContents.send("cpuInfo", cpu);
+      })
+      .catch((err) => {
+        console.log("cpu info error", err);
+      });
 
-	//CPU Info
-	si.cpu()
-		.then((cpu) => {
-			mainWindow.webContents.send("cpuInfo", cpu);
-		})
-		.catch((err) => {
-			console.log("cpu info error", err);
-		});
-
-	//File System Info
-	si.fsSize()
-		.then((disks) => {
-			// console.log(disks);
-			mainWindow.webContents.send("disks", disks);
-		})
-		.catch((err) => {
-			console.log("disks info error", err);
-		});
+    //File System Info
+    si.fsSize()
+      .then((disks) => {
+        //   console.log(disks);
+        mainWindow.webContents.send("disks", disks);
+      })
+      .catch((err) => {
+        console.log("disks info error", err);
+      });
+  });
 };
 
 ipc.on("invokeAction", function (event, data) {
-	// console.log(data);
-	// var result = app.getGPUFeatureStatus();
-	app.exit(0);
-	if (process.platform !== "darwin") app.quit();
+  // console.log(data);
+  // var result = app.getGPUFeatureStatus();
+  app.exit(0);
+  if (process.platform !== "darwin") app.quit();
 
-	// app.getGPUFeatureStatus()
+  // app.getGPUFeatureStatus()
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-	const displays = screen.getAllDisplays();
-	if (displays.length > 1) {
-		createWindow(displays[1].bounds.x + 50, displays[1].bounds.y + 50);
-	} else {
-		createWindow();
-	}
+  const displays = screen.getAllDisplays();
+  if (displays.length > 1) {
+    createWindow(displays[1].bounds.x + 50, displays[1].bounds.y + 50);
+  } else {
+    createWindow();
+  }
 
-	app.on("activate", function () {
-		// On macOS it's common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (BrowserWindow.getAllWindows().length === 0) {
-			const displays = screen.getAllDisplays();
-			if (displays.length > 1) {
-				createWindow(displays[1].bounds.x + 50, displays[1].bounds.y + 50);
-			} else {
-				createWindow();
-			}
-		}
-	});
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      const displays = screen.getAllDisplays();
+      if (displays.length > 1) {
+        createWindow(displays[1].bounds.x + 50, displays[1].bounds.y + 50);
+      } else {
+        createWindow();
+      }
+    }
+  });
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
-	app.exit(0);
-	if (process.platform !== "darwin") app.quit();
+  app.exit(0);
+  if (process.platform !== "darwin") app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
